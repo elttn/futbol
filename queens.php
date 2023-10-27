eab1e1382b52b5d87de1af418fd03de5d77d98d5
Toño Tafoya <ttn00741@gmail.com>
2023-10-26 22:14:49 -0600

    2da pagina

---
+++ queens.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Queens League</title>
+</head>
+
+<body data-bs-theme="ligth">
+    <div>
+        <nav class="navbar navbar-expand-lg lg-body-tertiary">
+            <div class="container-fluid">
+                <a href="queens.php" class="navbar-brand">Queens League </a>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link">Kings</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Americas</a>
+                        </li>
+                        <li class="nav-item">
+                            <button class="btn rounded-fill" onclick="cambiarTema()">
+                                <i>Modo Oscuro</i>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div>
+        <nav class="navbar navbar-expand-lg lg-body-tertiary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Equipos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Jugadores</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <main class="container py-4">
+        <header>
+            <div class="col-md-12">
+                <div class="card card-body text-light">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <img src="./Public/Images/Queens League/QL.png" alt="Queens League" class="img-fluid"
+                                style="width: 100%;">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container py-3">
+                    <div class="cold-md-8 ">
+                        <p class="justify-text">
+                            El fenómeno de la Kings League ha irrumpido con mucha fuerza en el panorama deportivo y de
+                            entretenimiento. Miles de seguidores se han visto atraídos por esta nueva competición,
+                            especialmente en espectadores jóvenes, por su dinamismo y las reglas diferentes al fútbol
+                            profesional. Tal es el interés generado que se ha desarrollado una división femenina llamada
+                            Queens League.
+
+                            El estreno de la Queens League es un acontecimiento muy esperado por los espectadores de la
+                            competición y por muchas jugadoras. Transcurrido un tiempo tras el anuncio de la creación de
+                            esta división femenina, ya han concluido las pruebas para la formación del plantel
+                            definitivo de futbolistas que la disputarán y está a punto de arrancar.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </header>
+
+        <div class="row py-2">
+            <div class="col-md-4">
+                <div class="card-bg-ligth">
+                    <div class="card-body">
+
+                        <div class="py-1">
+                            <img src="./Public/Images/Queens League/Presentacion.jpeg" alt="" style="width: 100%;">
+                        </div>
+
+                        <div class="py-3">
+                            <img src="./Public/Images/Queens League/Prsentacion2.jpg" alt="" style="width: 100%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="card-bg-light">
+                    <div class="card-body">
+                        <h1 class="style-letters">¿Cómo se juega?</h1>
+                        <p class="justify-text">
+                            Hablar de la Queens League como la división femenina de la Kings League es quedarse muy
+                            corto, ya que no hace justicia con la relevancia del torneo. Se puede definir a la Queens
+                            League como una competición de fútbol disputada por un total de 120 jugadoras, que se unen a
+                            las filas de 12 equipos distintos.
+
+                            Estos equipos son gestionados por streamers y diferentes creadoras de contenido. Sobre la
+                            fecha de inicio de la competición, hay que tomar dos referencias. En primer lugar, la
+                            celebración del Draft del que salen escogidas las 120 jugadoras del torneo, sin contar las
+                            futbolistas 11 y 12 de cada equipo.
+
+                            <br>
+                            Este Draft se celebró el 16 de abril de 2023 en el Cupra Arena. El photocall tuvo lugar a
+                            las 16:15h y la selección de las jugadoras a las 18h. En cuanto al inicio de la competición,
+                            comenzará el sábado 6 de mayo. Pese a que todavía no hay una hora de inicio concretada, se
+                            espera que sea el mismo horario que el de la Kings League, por lo que debería empezar a las
+                            16h.
+                        </p>
+
+                        <p class="justify-text">
+                            En el caso de organizarse algún evento preliminar para conmemorar el arranque de la Queens
+                            League, la competición podría arrancar antes de las 16h. Tanto la fase de grupos como las
+                            eliminatorias posteriores se disputarán los sábados.
+
+                            El formato de la Queens League consta de 11 jornadas, disputándose seis partidos seguidos
+                            cada sábado por la tarde. Serán encuentros de fútbol 7 con una corta duración, de 40
+                            minutos, con unas reglas novedosas cuyo objetivo principal es el de ofrecer espectáculo. Al
+                            finalizar la competición, los 8 mejores equipos clasificados disputarán el playoff.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <script src="./Public/js/bootstrap.min.js"></script>
+    <script src="./funciones.js"></script>
+</body>
+
+</html>
